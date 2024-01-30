d96e916f10c4eff65fef9563c4463f1c9f1a8e08
TuanHHH <hotuan1162@gmail.com>
2024-01-30 10:27:33 +0700

    them buoi 3

---
+++ Buổi 3/common_key.py
@@ -0,0 +1,18 @@
+dict1 ={'a':1,'b':2,'c':3}
+dict2 = {'b':3,'c':4,'d':5}
+def merge_dicts(dict1:dict,dict2:dict):
+    assert isinstance(dict1,dict) and isinstance(dict2,dict), f'2 parameters must be dictionary'
+    res_dict ={}
+    res_dict.update(dict1)
+    for k1,v1 in dict1.items():
+        for k2, v2 in dict2.items():
+            if k1 == k2:
+                res_dict[k1] += dict2[k2]
+    
+    for k2 in dict2:
+        if k2 not in res_dict:
+            res_dict[k2] = dict2[k2]
+
+    print(dict1)
+    print(res_dict)
+merge_dicts(dict1,dict2)
